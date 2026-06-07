91ff6bfc0215fc9d3a2e3c2b116daecf39597333
arenaconspiracy <gumbuknine@disroot.org>
2026-06-07 22:38:24 +0300

    Revert "Partial drill implementation (#7)"
    This reverts commit 2cea268fc4d7bcea2d084a44dce803d470c50a4f.

--- Content.Shared/_Drill/Drill/Components/DrillPortOutputOreComponent.cs
+++
@@ -1,9 +0,0 @@
-namespace Content.Shared._Drill.Drill.Components;
-
-/// <summary>
-/// A Drill port outputting ore items
-/// </summary>
-[RegisterComponent]
-public sealed partial class DrillPortOutputOreComponent : Component
-{
-}
