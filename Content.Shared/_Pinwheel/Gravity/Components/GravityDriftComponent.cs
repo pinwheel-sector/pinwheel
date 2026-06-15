3c72bbaa9618bd2c0c5a73257315ff10e08ba00a
arenaconspiracy <gumbuknine@disroot.org>
2026-06-15 12:36:12 +0300

    Janky proof of concept

---
+++ Content.Shared/_Pinwheel/Gravity/Components/GravityDriftComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Shared.Gravity;
+
+/// <summary>
+/// Marker component used by <see cref=GravityGeneratorSystem/>
+/// to drift entities in the direction of the station.
+/// </summary>
+[RegisterComponent]
+public sealed partial class GravityDriftComponent : Component
+{
+}
